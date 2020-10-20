3d04c89225ecba87cce6b911106303924ee31f1c
liguoliang <liguoliang@liguoliangdeMacBook-Pro-2.local>
2020-10-20 18:17:25 +0800

    2.0.4

--- Example/GLRouter/GLViewController.m
+++ Example/GLRouter/GLViewController.m
@@ -8,16 +8,17 @@
 
 #import "GLViewController.h"
 #import <UIViewController+Extension.h>
-@interface UISubButton:UIButton
+@interface UISubButton : UIButton
 
 @end
 @implementation UISubButton
 - (CGSize)intrinsicContentSize {
-    return CGSizeMake(CGRectGetWidth(self.bounds)+10, CGRectGetHeight(self.bounds)+2);
+    return CGSizeMake(CGRectGetWidth(self.bounds) + 10, CGRectGetHeight(self.bounds) + 2);
 }
+
 @end
 
-@interface GLViewController()
+@interface GLViewController ()
 @property (nonatomic) UILabel *label;
 @end
 
@@ -25,12 +26,13 @@
 #pragma mark- >> Build UI
 
 - (UILabel *)label {
-    if(!_label){
+    if (!_label) {
         _label = [[UILabel alloc]init];
         _label.font = [UIFont systemFontOfSize:28];
     }
     return _label;
 }
+
 - (UIStackView *)createStackViewForAxis:(UILayoutConstraintAxis)axis alignment:(UIStackViewAlignment)aligment distribution:(UIStackViewDistribution)distribution spacing:(CGFloat)spacing {
     UIStackView *__stack = [UIStackView new];
     __stack.spacing = spacing;
@@ -39,13 +41,14 @@
     __stack.distribution = distribution;
     return __stack;
 }
+
 - (UISubButton *)createButton:(NSString *)title action:(SEL)sel image:(UIImage *)img {
     UISubButton *b = [UISubButton buttonWithType:UIButtonTypeRoundedRect];
-    if(title){
+    if (title) {
         [b setTitle:title forState:UIControlStateNormal];
         [b setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
     }
-    if(img){
+    if (img) {
         [b setImage:img forState:UIControlStateNormal];
     }
     [b sizeToFit];
@@ -53,28 +56,28 @@
     [b addTarget:self action:sel forControlEvents:UIControlEventTouchUpInside];
     return b;
 }
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     [self setupRouter];
-    
+
     UIStackView *mainStackView = [self createStackViewForAxis:UILayoutConstraintAxisHorizontal alignment:UIStackViewAlignmentCenter distribution:UIStackViewDistributionFill spacing:10];
     mainStackView.frame = CGRectInset(self.view.bounds, 20, 20);
     [self.view addSubview:mainStackView];
-    
+
     UIStackView *vstack = [self createStackViewForAxis:UILayoutConstraintAxisVertical alignment:UIStackViewAlignmentLeading distribution:UIStackViewDistributionFillProportionally spacing:10];
     [mainStackView addArrangedSubview:vstack];
-    
+
     UIStackView *hs1 = [self createStackViewForAxis:UILayoutConstraintAxisHorizontal alignment:UIStackViewAlignmentCenter distribution:UIStackViewDistributionFillProportionally spacing:10];
-    hs1.frame = CGRectMake(0, 0, CGRectGetWidth(mainStackView.bounds)-20, 50);
-    
+    hs1.frame = CGRectMake(0, 0, CGRectGetWidth(mainStackView.bounds) - 20, 50);
+
     UIStackView *hs2 = [self createStackViewForAxis:UILayoutConstraintAxisHorizontal alignment:UIStackViewAlignmentCenter distribution:UIStackViewDistributionFillProportionally spacing:10];
-    hs2.frame = CGRectMake(0, 0, CGRectGetWidth(mainStackView.bounds)-20, 50);
-    
+    hs2.frame = CGRectMake(0, 0, CGRectGetWidth(mainStackView.bounds) - 20, 50);
+
     self.label.text = NSStringFromClass(self.class);
     [self.label sizeToFit];
     [vstack insertArrangedSubview:self.label atIndex:0];
-    
-    
+
     UILabel *tip = [UILabel new];
     tip.text = @"For URL:";
     [tip sizeToFit];
@@ -83,8 +86,7 @@
     [hs1 addArrangedSubview:[self createButton:@"Present" action:@selector(onClickPresentForURL) image:nil]];
     [hs1 addArrangedSubview:[self createButton:@"Invoke" action:@selector(onClickInvokeForURL) image:nil]];
     [vstack addArrangedSubview:hs1];
-    
-    
+
     UILabel *tip2 = [UILabel new];
     tip2.text = @"For KEY:";
     [tip2 sizeToFit];
@@ -93,26 +95,27 @@
     [hs2 addArrangedSubview:[self createButton:@"Present" action:@selector(onClickPresentForKey) image:nil]];
     [hs2 addArrangedSubview:[self createButton:@"Invoke" action:@selector(onClickInvokeForKey) image:nil]];
     [vstack addArrangedSubview:hs2];
-
 }
-
 
 #pragma mark- >> About Router
 /** router init */
 - (void)setupRouter {
-//    [GLRouterManager setVerifyScheme:@"lgl"];   // URL 跳转scheme限制(仅URL模式)
+    [GLRouterManager setVerifyScheme:@"lgl"];   // URL 跳转scheme限制(仅URL模式)
     [GLRouterManager managerWithRegisterFile:@"RouterList" withFromBundle:[NSBundle bundleForClass:self.class]]; // 路由表(仅Key模式)
 //    /* 拦截路由过程中发生的错误 */
-//    [GLRouterManager failure:^(NSError *error, NSString *detail) {
-//        NSLog(@"%@ - %@", error, detail);
-//    }];
+    [GLRouterManager failure: ^(NSError *error, NSString *detail) {
+        NSLog(@"%@ - %@", error, detail);
+    }];
 }
+
 - (void)onClickPushForURL {
     rto_dsp_clv(@"lgl://push/aboutViewController?text=Hello Router&image=https://www.baidu.com/img/PCtm_d9c8750bed0b3c7d089fa7d55720d6cf.png", nil, self);
 }
+
 - (void)onClickPresentForURL {
     rto_dsp(@"lgl://present/BlueViewController?text=Hello GLRouter&v1=1&v2=2", nil);
 }
+
 - (void)onClickInvokeForURL {
     rto_ivk(@"lgl://invoke/GLTools/sayHello:to:message:?v1=李雷&v2=韩梅梅&v3=Hi,Jerry I'm in GLRouter", ^(id ret) {
         showToastMsg(ret);
@@ -121,21 +124,21 @@
 
 - (void)onClickPushForKey {
     dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0), ^{
-        rto_dsp(@"toWeb", ^BOOL(id tgt) {
+        rto_dsp(@"toWeb", ^BOOL (id tgt) {
             [tgt setValue:@"http://www.baidu.com" forKey:@"url"];
             return YES;
         });
     });
 }
+
 - (void)onClickPresentForKey {
     rto_dsp(@"presentAbout", nil);
 }
+
 - (void)onClickInvokeForKey {
     rto_ivk(@"sayHello", ^(id ret) {
         showToastMsg(ret);
     });
 }
-
-
 
 @end
