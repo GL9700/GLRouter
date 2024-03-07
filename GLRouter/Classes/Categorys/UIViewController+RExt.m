6dc08084946b1a2c6a7792938143046dc3ef2af8
liguoliang <36617161@qq.com>
2024-03-07 20:33:14 +0800

    ver 2.1.2: 增加了两个方法，防止接收数据的时候kvc问题

--- GLRouter/Classes/Categorys/UIViewController+RExt.m
+++ GLRouter/Classes/Categorys/UIViewController+RExt.m
@@ -8,7 +8,16 @@
 #import <GLRouter/UIViewController+RExt.h>
 
 @implementation UIViewController (RExt)
+
 - (void)routerParams:(NSDictionary *)params {
+	// 这里接收路由中的Query参数
 }
 
+- (void)setNilValueForKey:(NSString *)key {
+	NSLog(@" --- --- GLRouter : 给一个Key赋空值了 --- --- ");
+}
+
+- (void)setValue:(id)value forUndefinedKey:(NSString *)key {
+	NSLog(@" --- --- GLRouter : 给一个未知的Key赋值了 --- --- ");
+}
 @end
